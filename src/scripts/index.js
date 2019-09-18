36b465dc8a5dea013c0d2380afea9a74dd63dea6
Fran Borg <francesca.bte@outlook.com>
2019-09-18 15:27:30 +0200

    Add starting files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,23 @@
-// The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
-// Import any additional modules you want to include below \/
+import "../styles/main.scss";
 
+const modal = document.getElementById("myModal");
+let promiseOfModal = new Promise(function (resolve) {
+    window.setTimeout(function () {
+        resolve(modal)
+    }, (1000 * 60));
+});
 
-// \/ All of your javascript should go here \/
+promiseOfModal.then(function(val) {
+    console.log("User has been on the page for 60 seconds");
+    val.style.display = "block";
+})
+
+modal.addEventListener("click", (e) => {
+    switch(e.target.className) {
+        case "close":
+        case "modal":
+        modal.style.display = "none"
+        break;
+    }
+})
 
